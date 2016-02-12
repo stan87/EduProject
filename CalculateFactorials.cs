05e82aa6cc1c1afcc6c610bd224f212c3b0030e6
stan87 <rare_breed_bg@yahoo.com>
2016-02-12 23:14:49 +0200

    Calculate 1 + 1!/X + 2!/X2 + … + N!/XN

---
+++ CalculateFactorials.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class CalculateFactorials
+    {
+        static void Main()
+        {
+
+        while (true)
+        {
+            Console.WriteLine("Enter number N:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number X:");
+            int x = int.Parse(Console.ReadLine());
+
+            double power = 1;
+            double factorial = 1;
+            double sum = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                power *= x;
+                sum += (factorial / power);
+            }
+
+            Console.WriteLine("{0:F5}", sum);
+        }
+
+        }
+    }
+
